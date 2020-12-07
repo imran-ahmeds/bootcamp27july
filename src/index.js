9102d47837ac8dea507994432e230cccfd8cfd18
Imran Ahmed Siddiqui <37952664+imran-ahmeds@users.noreply.github.com>
2020-12-08 00:45:11 +0500

    correction
    index.js corrected

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import Counter from './count';
+import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
