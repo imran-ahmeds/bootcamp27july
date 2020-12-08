2427d28aa1d5321ed45752dea56c80b4643b5e35
Imran Ahmed Siddiqui <37952664+imran-ahmeds@users.noreply.github.com>
2020-12-08 11:15:46 +0500

    css
    styling

--- src/count.js
+++ src/count.js
@@ -1,14 +1,14 @@
 import React, {useState} from 'react'
 import {Message} from './Message';
-
+import './App.css';
 function Counter(){
     let [count, setCount] = useState(0);
     return(
-        <div>
+        <div className='box'>
             <Message setCounter= {count}/>
             <input type="number" value = {count} className="add"></input>
             <input type="button" value="submit" 
-            onClick= {()=> setCount (count += parseInt({count}))}/>
+            onClick= {()=> setCount (count += parseInt(count))}/>
                 
         </div>
     );
